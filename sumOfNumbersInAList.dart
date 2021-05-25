077f380cf1da0a9719cc2a3ee45097b132d2bf08
noahyalla <noahyalla@gmail>
2021-05-25 20:01:40 +1000

    sum of numbers in a list

---
+++ sumOfNumbersInAList.dart
@@ -0,0 +1,25 @@
+
+
+void main(){
+
+var values = [1,3,5,7,9];
+var sum = 0;
+for (var value in values){
+sum+=value;
+}
+print(sum);
+}
+
+
+
+
+
+
+
+
+
+//Exercise
+
+//Given the list of integers: [1,3,5,7,9]
+
+//Write a program that prints the sum of all these values
